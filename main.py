acfc41ee8c5ced00069c9f6b177a03e3e5ae14a1
Yann Dirson <ydirson@free.fr>
2024-06-17 23:32:18 +0200

    Rate-limiting using slowapi

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import http.client
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Request, Response
 
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func=get_remote_address, default_limits=["5/minute"])
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 HOST = 'army-forge.onepagerules.com'
 JSON = "application/json"
 
 @app.get("/")
-def get_root():
-    return {"Version": "0.0.1"}
+def get_root(request: Request):
+    return {"Version": "0.0.2"}
 
 @app.get("/opr/api/rules/common/{id}")
-def get_commonrules(id: str):
+def get_commonrules(request: Request, id: str):
     conn = http.client.HTTPSConnection(HOST)
     conn.request("GET", f"/api/rules/common/{id}", headers={"Host": HOST})
     response = conn.getresponse()
